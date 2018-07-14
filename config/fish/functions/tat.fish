3623665af0ecc4261861d10ad487fd78ef68eff5
Chris Thorn <chris@thorn.co>
2018-07-13 17:17:39 -0700

    fish: better tat function
    `$TMUX` is always empty when `tat` is invoked outside of tmux. Since I
    don't use `tat` from within tmux, using it as indicator of how to behave
    isn't very useful. With the current logic, `tat` creates a new session
    every time it's invoked.
    
    This flips the logic to check for the session by name, attaching to it
    if it exists and creating it otherwise.  If invoked within tmux, it
    aborts.

--- config/fish/functions/tat.fish
+++ config/fish/functions/tat.fish
@@ -3,17 +3,20 @@ function tat
   set child (basename $PWD | tr . -)
   set session_name $parent/$child
 
-  if test -z $TMUX
+  if test -n "$TMUX"
+    echo "leave tmux first" >&2
+    return 1
+  end
+
+  set session_exists (tmux ls -F '#S' | grep "$session_name")
+
+  if test -n "$session_exists"
+    tmux attach -t $session_name
+  else
     tmux new-session -As $session_name -n code -d
     tmux new-window -t $session_name -n server
     tmux new-window -t $session_name -n console
     tmux select-window -t $session_name -n
     tmux attach -t $session_name
-  else
-    set session exists (tmux list-sessions | sed -E 's/:.*$//' | grep -q ^$session_name$)
-    if not session_exists
-      eval TMUX='' tmux new-session -Ad -s $session_name
-    end
-    tmux switch-client -t $session_name
   end
 end
