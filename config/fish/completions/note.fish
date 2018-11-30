849ae50339c5f2ca182ffba13e7a086b68081898
Chris Thorn <chris@thorn.co>
2018-11-30 10:35:22 -0800

    Note: add `last` subcommand
    Opens the last note file for the given tag. Makes an assumption that
    only note files live in the directory, which is probably fine, but it'd
    be better to not do that. One day I'll get around to cleaning all this
    up.

--- config/fish/completions/note.fish
+++ config/fish/completions/note.fish
@@ -1,5 +1,6 @@
 set -l list_tags "(note tags)"
 
-complete -c note -f -n "__fish_use_subcommand" -a "new tags"
+complete -c note -f -n "__fish_use_subcommand" -a "last new tags"
+complete -c note -f -n "__fish_seen_subcommand_from last" -a $list_tags
 complete -c note -f -n "__fish_seen_subcommand_from new" -a $list_tags
 complete -c note -f -n "__fish_seen_subcommand_from tags" -a ""
