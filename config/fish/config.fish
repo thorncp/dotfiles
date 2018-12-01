cdfd8d7cdbada1a642144ae9129de2ea9f5f7962
Chris Thorn <chris@thorn.co>
2018-11-30 16:41:27 -0800

    Fish: don't append to PATH in tmux
    In tmux I'm seeing these path entries duplicated. I don't know if this
    solution is ideal, but it seems to work.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -15,9 +15,11 @@ set -x PIPENV_DONT_LOAD_ENV 1
 set -x SHELL (which fish)
 set -x VISUAL vim
 
-set -x PATH $PATH $GOPATH/bin
-set -x PATH $HOME/.bin $PATH
-set -x PATH .git/safe/../../bin $PATH
+if not set -q TMUX
+  set -x PATH $PATH $GOPATH/bin
+  set -x PATH $HOME/.bin $PATH
+  set -x PATH .git/safe/../../bin $PATH
+end
 
 alias ag "ag --pager less --color-line-number '1;34'"
 alias g git
