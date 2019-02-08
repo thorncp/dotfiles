6147f0d21679125f32f533a7517edd3d49f5939e
Chris Thorn <chris@thorn.co>
2019-02-08 14:57:45 -0800

    Homebrew: remove upgrade cleanup env var
    Homebrew 2.0 does some cleanup automatically and running `brew upgrade`
    fails when this option is set.
    
    > Error: Calling 'HOMEBREW_UPGRADE_CLEANUP' is disabled! There is no
    > replacement.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,7 +6,6 @@ set -x EDITOR vim
 set -x FZF_DEFAULT_COMMAND 'ag -g ""'
 set -x GOPATH $HOME/go
 set -x HOMEBREW_NO_AUTO_UPDATE 1
-set -x HOMEBREW_UPGRADE_CLEANUP 1
 set -x LESS "--RAW-CONTROL-CHARS --no-init --quit-if-one-screen"
 set -x PIPENV_DONT_LOAD_ENV 1
 set -x SHELL (which fish)
