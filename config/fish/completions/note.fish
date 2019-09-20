840721f9458bb99aaa83d590de7b9eea1d96c844
Chris Thorn <chris@thorn.co>
2019-09-20 09:21:44 -0700

    Note: `commands` does not have any completions
    The default will complete file paths from the current directory.

--- config/fish/completions/note.fish
+++ config/fish/completions/note.fish
@@ -2,6 +2,7 @@ set -l list_commands "(note commands)"
 set -l list_tags "(note tags)"
 
 complete -c note -f -n "__fish_use_subcommand" -a $list_commands
+complete -c note -f -n "__fish_seen_subcommand_from commands" -a ""
 complete -c note -f -n "__fish_seen_subcommand_from last" -a $list_tags
 complete -c note -f -n "__fish_seen_subcommand_from new" -a $list_tags
 complete -c note -f -n "__fish_seen_subcommand_from tags" -a ""
