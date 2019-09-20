7542ccec804731e0c16bc4320eb6266c758f2aed
Chris Thorn <chris@thorn.co>
2019-09-20 09:19:39 -0700

    Note: Add command to list commands
    Use this command for subcommand tab completion.

--- config/fish/completions/note.fish
+++ config/fish/completions/note.fish
@@ -1,6 +1,7 @@
+set -l list_commands "(note commands)"
 set -l list_tags "(note tags)"
 
-complete -c note -f -n "__fish_use_subcommand" -a "last new tags"
+complete -c note -f -n "__fish_use_subcommand" -a $list_commands
 complete -c note -f -n "__fish_seen_subcommand_from last" -a $list_tags
 complete -c note -f -n "__fish_seen_subcommand_from new" -a $list_tags
 complete -c note -f -n "__fish_seen_subcommand_from tags" -a ""
