e59e6b0e0c2eeb2554c84ed664dbfbce55a9cc0b
Chris Thorn <chris@thorn.co>
2023-07-30 22:49:22 -0500

    Fish: fix specdiff
    Not sure if Git changed the behavior of search patterns at some point,
    but the one here no longer works. Enabling enhanced regex and removing
    the escape char seems to fix it.
    
    Also adds a couple more RSpec terms to look for and discards commented
    lines.

--- .config/fish/functions/specdiff.fish
+++ .config/fish/functions/specdiff.fish
@@ -1,9 +1,9 @@
 function specdiff -d "Diff RSpec between two Git trees" -a left right
-  set -l pattern "describe\|it\|context\|specify"
+  set -l pattern "describe|it|context|specify|feature|scenario"
   test -z $left; and set -l left (git main-local)
   test -z $right; and set -l right 'HEAD'
 
   $DIFF -b -U 3 \
-    (git grep -w -h --no-line-number --no-color -e $pattern $left -- spec/ | psub) \
-    (git grep -w -h --no-line-number --no-color -e $pattern $right -- spec/ | psub)
+    (git grep -w -h --no-line-number --no-color -Ee $pattern $left -- spec/ | ag -v '^\s+#'| psub) \
+    (git grep -w -h --no-line-number --no-color -Ee $pattern $right -- spec/ | ag -v '^\s+#'| psub)
 end
